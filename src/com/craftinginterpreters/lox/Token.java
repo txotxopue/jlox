b0ceb6abe7ca29a535bd1ed025d8327d44a7758e
David <elchicoconinsomnio@gmail.com>
2017-02-12 21:17:36 +0000

    Initial commit. Prompt and Token.

---
+++ src/com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,22 @@
+package com.craftinginterpreters.lox;
+
+public class Token 
+{
+	final TokenType type;
+	final String lexeme;
+	final Object literal;
+	final int line;
+	
+	Token (TokenType type, String lexeme, Object literal, int line)
+	{
+		this.type = type;
+		this.lexeme = lexeme;
+		this.literal = literal;
+		this.line;
+	}
+	
+	public String ToString ()
+	{
+		return type + " " + lexeme + " " + literal;
+	}
+}
